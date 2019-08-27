ed8e8014f46da404db6cf2398f8d8cacfec928ba
agreyson <agreyson@castilleja.org>
2019-08-26 18:32:06 -0700

    Initial Commit

---
+++ RockPaperScissors/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RockPaperScissors
+//
+//  Created by Ann Greyson on 8/26/19.
+//  Copyright © 2019 Greyson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
